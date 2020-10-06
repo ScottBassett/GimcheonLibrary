820be2572755a5469c927b79396cd00c3a92d6c3
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2020-10-06 15:41:28 +0100

    Added authors controller

---
+++ GimcheonLibrary.Web/Controllers/AuthorsController.cs
@@ -0,0 +1,116 @@
+﻿using GimcheonLibrary.DataAccess.Models;
+using GimcheonLibrary.DataAccess.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace GimcheonLibrary.Web.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly AuthorRepository _authorRepository;
+
+        public AuthorsController(IConfiguration configuration)
+        {
+            _authorRepository = new AuthorRepository(configuration);
+        }
+
+        // GET: AuthorsController
+        public IActionResult Index()
+        {
+            return View(_authorRepository.FindAll());
+        }
+
+        // GET: AuthorsController/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Author author = _authorRepository.FindById(id.Value);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
+        }
+
+        // GET: AuthorsController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AuthorsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                _authorRepository.Add(author);
+                return RedirectToAction("Index");
+            }
+
+            return View(author);
+        }
+
+        // GET: AuthorsController/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Author author = _authorRepository.FindById(id.Value);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
+        }
+
+        // POST: AuthorsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                _authorRepository.Update(author);
+                return RedirectToAction("Index");
+            }
+
+            return View(author);
+        }
+
+        // GET: AuthorsController/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _authorRepository.Remove(id.Value);
+            return RedirectToAction("Index");
+        }
+
+        // POST: AuthorsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
